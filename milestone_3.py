79a20ceeff956efec26f5dc27cf1cf32a5d83ca2
sophieelg <sophie2592@hotmail.com>
2023-01-05 17:43:24 +0000

    only while loop

--- milestone_3.py
+++ milestone_3.py
@@ -1,24 +1,9 @@
 # %%
-import random
-
-# define possible words
-
-word_list = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Grape']
-print(word_list)
-# %%
-
-# select random word from list
-
-word = random.choice(word_list)
-print(word)
-# %%
-
-# while loop until user enters a single character
 
 while True:
-    guess = input('Guess a single letter.')
-    if len(guess) == 1:
+    guess = input('Guess a single letter')
+    if len(guess) ==1 and guess.isalpha():
         break
     else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
+        print('Invalid letter. Please enter a single alphabetical character.')
 # %%
