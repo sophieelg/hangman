3ae2d566ee962813b4ba831a33874cd3411ea2f7
sophieelg <sophie2592@hotmail.com>
2023-01-05 12:26:44 +0000

    print random word from list

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,13 @@
+# %%
+import random
+
 # define possible words
 
 word_list = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Grape',]
 print(word_list)
+# %%
+
+# print random word from list
+
+word = random.choice(word_list)
+print(word)
