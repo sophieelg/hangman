2d0a6b0d9964c62b91a8392a801556fa74959a72
sophieelg <sophie2592@hotmail.com>
2023-01-05 16:05:23 +0000

    while loop to check if the input is a valid guess

---
+++ milestone_3.py
@@ -0,0 +1,9 @@
+# %%
+
+while True:
+    guess = input('Guess a single letter')
+
+    if len(guess) ==1 and guess.isalpha():
+        break
+    else:
+        print('Invalid letter. Please enter a single alphabetical character.')
