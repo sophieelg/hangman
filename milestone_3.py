2eea934dbea32715c41f21d7206955b544b7c94a
sophieelg <sophie2592@hotmail.com>
2023-01-05 18:52:30 +0000

    adds guess in word

--- milestone_3.py
+++ milestone_3.py
@@ -6,3 +6,9 @@ while True:
         break
     else:
         print('Invalid letter. Please enter a single alphabetical character.')
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+# %%
