707dd0fb4537b23bcdd500a52597887bbe99a0d2
sophieelg <sophie2592@hotmail.com>
2023-01-05 16:47:17 +0000

    ilestone_3.py
    takes out code

--- milestone_3.py
+++ milestone_3.py
@@ -20,11 +20,3 @@ while True:
         break
     else:
         print('Invalid letter. Please enter a single alphabetical character.')
-
-# checks whether the guess is in the word
-
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
-# %%
