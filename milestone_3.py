3811d29cd384964424ef086acf22588265d6d83a
sophieelg <sophie2592@hotmail.com>
2023-01-05 16:33:28 +0000

    checks whether the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,15 @@
 # %%
+import random
+
+# define possible words
+
+word_list = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Grape']
+
+# select random word from list
+
+word = random.choice(word_list)
+
+# while loop until user enters a single character
 
 while True:
     guess = input('Guess a single letter')
@@ -7,3 +18,11 @@ while True:
         break
     else:
         print('Invalid letter. Please enter a single alphabetical character.')
+
+# checks whether the guess is in the word
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+# %%
