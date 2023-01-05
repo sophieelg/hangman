d7d590fedc2647ca76f6f13234d4f6947f8750c7
sophieelg <sophie2592@hotmail.com>
2023-01-05 17:17:55 +0000

    while loop

--- milestone_3.py
+++ milestone_3.py
@@ -4,14 +4,16 @@ import random
 # define possible words
 
 word_list = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Grape']
+print(word_list)
+# %%
 
 # select random word from list
 
 word = random.choice(word_list)
+print(word)
+# %%
 
 # while loop until user enters a single character
-
-# %%
 
 while True:
     guess = input('Guess a single letter.')
