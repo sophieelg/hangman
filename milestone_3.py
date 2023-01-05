b397c42a060282186c8aed5e6e669eb1a23b3fe1
sophieelg <sophie2592@hotmail.com>
2023-01-05 18:09:13 +0000

    checks if guess is in word

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,10 @@
 # %%
+from milestone_2 import word_list
+from milestone_2 import word
+
+# %%
+
+# while loop checks if input is valid guess
 
 while True:
     guess = input('Guess a single letter')
@@ -6,4 +12,12 @@ while True:
         break
     else:
         print('Invalid letter. Please enter a single alphabetical character.')
+
+# checks whether the guess is in the word
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
 # %%
