8ce4469a57dd10056df7e3e2e336f4f34c82a370
sophieelg <sophie2592@hotmail.com>
2023-01-11 19:11:15 +0000

    adds num_letters attribute

--- milestone_4.py
+++ milestone_4.py
@@ -2,12 +2,11 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word_list = word_list # a list of words
-        self.num_lives = num_lives # the number of lives the player has at the start of the game
-
         self.word = random.choice(word_list) # the word to be guessed
         self.word_guessed = len(self.word) * ['_'] # a list of the letters not yet guessed
-        self.num_letters = None # the number of unique letters in the word that have not been guessed yet
+        self.num_letters = len(set(self.word)) # the number of unique letters in the word that have not been guessed yet
+        self.num_lives = num_lives # the number of lives the player has at the start of the game
+        self.word_list = word_list # a list of words
         self.list_of_guesses = [] # a list of the guesses that have already been tried.
 
         word_list = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Grape']
