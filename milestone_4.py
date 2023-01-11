f9d1e0514b243e51684f4333296223d89f08a2af
sophieelg <sophie2592@hotmail.com>
2023-01-11 18:59:44 +0000

    adds word_guessed attribute

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,7 @@ class Hangman:
         self.num_lives = num_lives # the number of lives the player has at the start of the game
 
         self.word = random.choice(word_list) # the word to be guessed
-        self.word_guessed = None # a list of the letters not yet guessed
+        self.word_guessed = len(self.word) * ['_'] # a list of the letters not yet guessed
         self.num_letters = None # the number of unique letters in the word that have not been guessed yet
         self.list_of_guesses = [] # a list of the guesses that have already been tried.
 
