bf14b68cee1ca18323cc659be37cd57c54ef4e21
sophieelg <sophie2592@hotmail.com>
2023-01-12 15:28:51 +0000

    adding self to word_list

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,7 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list) # the word to be guessed
+        self.word = random.choice(self.word_list) # the word to be guessed
         self.word_guessed = len(self.word) * ['_'] # a list of the letters not yet guessed
         self.num_letters = len(set(self.word)) # the number of unique letters in the word that have not been guessed yet
         self.num_lives = num_lives # the number of lives the player has at the start of the game
