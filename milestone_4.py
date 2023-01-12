a394a574a5c23785d9d01ead30e320eabc838cc8
sophieelg <sophie2592@hotmail.com>
2023-01-12 20:51:34 +0000

    moves append

--- milestone_4.py
+++ milestone_4.py
@@ -16,18 +16,19 @@ class Hangman:
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
+            print(f"Good guess! {guess} is in the word.")            
     
     def ask_for_input(self):
         while True:
-            guess = input("Guess a single letter.")
-            self.list_of_guesses.append(guess)
+            guess = input("Guess a single letter.")                      
             if len(guess) !=1 or not guess.isalpha():
-                print("Invalid letter. Please, enter a single alphabetical character.")
+                print("Invalid letter. Please, enter a single alphabetical character.")        
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else: 
+                self.list_of_guesses.append(guess)
                 break
+        
         self.check_guess(guess)
                 
 
