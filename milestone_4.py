2f098ee3bbeda8787b49e7b6c1df4832cd34a27c
sophieelg <sophie2592@hotmail.com>
2023-01-16 21:08:30 +0000

    undo

--- milestone_4.py
+++ milestone_4.py
@@ -16,12 +16,7 @@ class Hangman:
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
-            for letter in self.word:
-                if letter == guess:
-                  self.word_guessed[letter] = letter
-        self.num_letters -= 1
-                    
+            print(f"Good guess! {guess} is in the word.")            
                 
     def ask_for_input(self):
         while True:
