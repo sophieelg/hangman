cd5d74b020ab4020add1a27cc9211b74958b93ff
sophieelg <sophie2592@hotmail.com>
2023-01-17 15:38:07 +0000

    changed order of code

--- milestone_4.py
+++ milestone_4.py
@@ -24,8 +24,8 @@ class Hangman:
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word.")
             print (f"You have {self.num_lives} lives left.")
-        self.list_of_guesses.append(guess)
         self.num_letters -= 1
+        self.list_of_guesses.append(guess)
                
     def ask_for_input(self):
         while True:
